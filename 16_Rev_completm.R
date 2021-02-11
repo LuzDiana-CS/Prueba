b57426212f890e58c30273becf8950ea6becb336
LuzDiana-CS <luzdianacs@gmail.com>
2021-02-11 16:35:43 -0500

    Completar data

---
+++ 16_Rev_completm.R
@@ -0,0 +1,23 @@
+### Completacion de datos mensuales faltantes
+### MÃ©todo CUTOFF (Feng et al, 2014): Imputacion optimizada de una correlacion cruzada
+### http://dx.doi.org/10.1016/j.jhydrol.2014.11.012
+### https://github.com/hydrocodes
+rm(list=ls())
+dev.off()##borrado de gráficos
+library(cutoffR)
+# Ingresar una base de datos en csv: %b-%y (date), datos mensuales (cabecera con codigo de estaciones)
+data <- read.csv("E:/curso_R/4_CompletarDatos/16_datam.csv",header=TRUE, check.names = F, stringsAsFactors = F)
+# Ingresar la ruta y el nombre del archivo de salida
+output <- "E:/curso_R/4_CompletarDatos/completm.csv"
+
+# Funcion de completacion
+completm <- function(data)
+{fechas <- as.POSIXct(paste("01", data$date, sep = "-"), format = "%d-%b-%y")
+ data$date <- fechas
+ comm <- cutoff(data, method = c("correlation"), corr = "spearman", cutoff = 0.7)
+ rownames(comm) <- as.factor(data$date)
+ write.csv(comm,output)
+}
+
+# Ejecutar funcion
+completm(data)
